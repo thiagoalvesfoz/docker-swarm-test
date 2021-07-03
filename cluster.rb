fa1f4e43e53885ae87ccb92f04313e79d2371e88
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-07-03 18:51:16 -0300

    fix: :zap: script to init docker swarm and add works

--- cluster.rb
+++ cluster.rb
@@ -1,12 +1,5 @@
 
 @clusters = {
-  "manager" => { 
-    :box_image => "generic/ubuntu1804", 
-    :ip => "192.168.10.10", 
-    :mem => 1024,
-    :script => "./script.sh", # instalar docker
-    # :folder => "./app", # pra depois
-  },
   "node1" => { 
     :box_image => "generic/ubuntu1804",
     :ip => "192.168.10.11",
@@ -18,5 +11,12 @@
     :ip => "192.168.10.12", 
     :mem => 1024,
     :script => "./script.sh",
-  }
+  },
+  "manager" => { 
+    :box_image => "generic/ubuntu1804", 
+    :ip => "192.168.10.10", 
+    :mem => 1024,
+    :script => "./manager.sh",
+    # :folder => "./app", # pra depois
+  },
 }
--- manager.sh
+++ manager.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get clean && sudo apt-get update
 
 # ##################################################################################################################
@@ -20,5 +22,33 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# EXECUTE WITHOUT SUDO
+# EXECUTE KEYCLOAK
 usermod -aG docker vagrant
+
+# INSTALL sshpass
+sudo apt-get install sshpass -y
+
+## CONFIGS DOCKER SWARM
+MANAGER="192.168.10.10"
+HOSTS=("192.168.10.11" "192.168.10.12")
+KEY="vagrant"
+
+# INIT DOCKER SWARM
+docker swarm init --advertise-addr $MANAGER
+
+# GET TOKEN
+TOKEN=$(docker swarm join-token worker -q)
+
+# GENERATE COMMAND TO JOIN WORKS
+JOIN_WORKER="docker swarm join --token ${TOKEN} ${MANAGER}:2377"
+
+for i in ${!HOSTS[*]} ; do
+     sshpass -p ${KEY} ssh -t -o StrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -l ${KEY} ${HOSTS[i]} "${JOIN_WORKER}"
+   
+    if [ $? -eq 0 ] ; then
+        echo "WORKER ${HOSTS[i]} ADICIONADO"
+    fi
+done
+
+# REMOVE SSHPASS
+sudo apt-get remove sshpass -y
